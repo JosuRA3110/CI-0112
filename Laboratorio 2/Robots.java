1e6be214185cccdba5afe53cbea3b06be6dfee81
Josué Rodríguez <josurodriag@gmail.com>
2025-04-27 22:03:47 -0600

    Actualizacion de codigo, se supone que terminado sin pts extra

---
+++ Laboratorio 2/Robots.java
@@ -0,0 +1,51 @@
+//Robots
+public class Robots{
+
+    //Atributos
+    private String nombre;
+    private int puntosVida;
+    private int ataque;
+
+    //Metodo Constructor
+    public Robots(String nombre, int puntosVida, int ataque){
+        this.nombre= nombre;
+        this.puntosVida= puntosVida;
+        this.ataque= ataque;
+    }
+
+    //Getters
+    public String getNombre(){
+        return nombre;
+    }
+    public int getPuntosdeVida(){
+        return puntosVida;
+    }
+    public int getAtaque(){
+        return ataque;
+    }
+
+    //Setters
+    public void setNombre(String nombre){
+        this.nombre= nombre;
+    }
+    public void setPuntosdeVida(int puntosdeVida){
+        this.puntosdeVida= puntosdeVida;
+    }
+    public void setAtaque(int ataque){
+        this.ataque= ataque;
+    }
+
+    //Metodos solicitados
+    public void atacar(Robot otroRobot){//Note que aca ya estamos suponiendo que existen dos objetos que son los robots que estaran activos para el encuentro por realizar//
+        if (ataque>=10 && ataque<=20){
+            otroRobot.puntosVida-= this.ataque;
+            System.out.println(this.nombre + " ha realizado el ataque al robot "+ otroRobot.nombre + "los puntos de vida restantes de"+ otroRobot.nombre + " son de "+ otroRobot.puntosVida);
+        }else{
+            System.out.println("Digite nuevamente el ataque de "+ this.nombre + " recuerde que debe de ser un numero entre 10 y 20 :)");
+        }
+    }//*Okay, para este punto solo atacamos entre los robots pero no sabemos si siguen vivos, es decir tienen puntos de vida, incluire una verificacion de ello, como se solicita en las indicaciones*/
+    
+    public boolean estaVivo(){//Note que esta funcion facilitara el verificar si los robots estan vivos o fueron destruidos para la clase juegosdebatalla, pues ya sabremos cuando un robot sigue con vida (si es true) o si fue destruido (si el boolean es falso)//
+        return this.puntosVida > 0;
+    }
+}
