7dedac3d25d58a6c59dff358b1e20a4b6fcd45dc
Josué Rodríguez <josurodriag@gmail.com>
2025-05-06 22:53:42 -0600

    Se sube la carpeta proyecto1 y la clase batalla naval para dar inicio al proyecto

---
+++ Proyecto1/BatallaNaval.java
@@ -0,0 +1,11 @@
+//Juego Batalla Naval 
+/*Cada jugador cuenta con un tablero de 5x5 y debe colocar manualmente tres barcos de una 
+casilla en su propio tablero. Los jugadores se turnan para disparar una coordenada (fila y 
+columna) al tablero del oponente. Si aciertan, deben marcar el impacto y continúan su turno; si 
+fallan, el turno pasa al otro jugador. El primer jugador en hundir los tres barcos del oponente 
+gana la partida. El programa debe mostrar ambos tableros: el del jugador actual (con sus 
+barcos e impactos recibidos) y el tablero oculto del oponente (mostrando solo los disparos 
+realizados y sus resultados). */
+public class BatllaNaval{
+
+}
