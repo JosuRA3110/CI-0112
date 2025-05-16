bf6e33dedd3787485623759fec84d797d9d6a8a9
KevinCalderon <elkevin2811@gmail.com>
2025-05-16 02:33:41 -0600

    Interfaz con el Doxygen
    Se actualiza la Interfaz con los comentarios.

--- Proyecto1/Interfaz.java
+++ Proyecto1/Interfaz.java
@@ -1,15 +1,28 @@
 import java.util.Scanner;
 public class Interfaz{
+    
+    /**
+     * @brief se declara el booleano jugar, que nos permite crear el ciclo que se cierra unicamente cuando el usuario ya no quiere jugar.
+     * el usuario decide que jugar mediante el scanner.
+     * si marca 1, se instancia un objeto de BatallaNaval, y se llama al metodo de jugar la batalla naval.
+     * si marca 2, se llama al metodo de dosRondas de Ahorcado, e inicia el juego.
+     * si marca 3 termina el programa.
+     * 
+     * Luego de jugar algun juego, se le pregunta al usuario si quiere volver a jugar.
+     * si marca 1, se vuelve a la interfaz inicial.
+     * si marca 2, se cierra el programa.
+     */
     public static void main(String[] args){
 
         Scanner scanner = new Scanner(System.in);
         boolean jugar = true;
 
+        
         while(jugar == true){
             System.out.println("------------------------");
             System.out.println("BIENVENIDO A LA INTERFAZ");
             System.out.println("------------------------");
-            System.out.println("Con cual juego desea inciar?");
+            System.out.println("Con cual juego desea empezar?");
             System.out.println("------------------------");
             System.out.println("1 - Batalla Naval");
             System.out.println("2 - Ahorcado");
@@ -18,12 +31,10 @@ public class Interfaz{
 
 
             if (opcion == 1){
-                System.out.println();
                 System.out.println("Iniciando Batalla Naval...");
                 BatallaNaval jugarBatallaNav = new BatallaNaval();
                 jugarBatallaNav.jugarBatallaNaval();
             }else if (opcion == 2){
-                System.out.println();
                 System.out.println("Iniciando Ahorcado");
                 Ahorcado jugarAhorcado = new Ahorcado();
                 jugarAhorcado.dosRondas();
